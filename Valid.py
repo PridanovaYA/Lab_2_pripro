1d3b8713ffa385b68681a8be282767b1d0f4e2c5
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2021-12-04 21:01:56 +0400

    Шаблон класса проверки на валидность записей

---
+++ Valid.py
@@ -0,0 +1,35 @@
+import tqdm
+"""класс для проверки данных на валидность записи"""
+#        "telephone": "+7-(918)-043-20-33",
+#        "height": "1.76",
+#       "inn": "920207713870",
+#        "passport_number": 123166,
+#        "university": "Санкт-Петербургский государственный архитектурно-строительный университет",
+#        "age": 63,
+#        "academic_degree": "Магистр",
+#        "worldview": "Пантеизм",
+#        "address": "Аллея Прудовой 766"
+class Valid:
+    val: list
+    def __init__(self, array) -> None:
+        self.val = array
+
+    def check_valid(self):
+        result = {}
+
+        return result
+
+    def count_correct_records(self):
+        count_correct = 0
+
+
+        return count_correct
+
+    def count_incorrect_records(self):
+        count_incorrect = 0
+
+        return count_incorrect
+
+
+
+
