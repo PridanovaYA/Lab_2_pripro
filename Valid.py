ed23f7668593f977922dc491555848f6e6bce5c1
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2021-12-04 23:42:01 +0400

    Проверки телефона,роста,инн,номера паспорта, университета, возраста, академической степени, вероисповедания, адреса. Не полные.

--- Valid.py
+++ Valid.py
@@ -1,4 +1,5 @@
 import tqdm
+import re
 """класс для проверки данных на валидность записи"""
 #        "telephone": "+7-(918)-043-20-33",
 #        "height": "1.76",
@@ -10,12 +11,78 @@ import tqdm
 #        "worldview": "Пантеизм",
 #        "address": "Аллея Прудовой 766"
 class Valid:
+
     val: list
-    def __init__(self, array) -> None:
+    dictionary: dict
+
+    def __init__(self, array,a) -> None:
+
+        self.dictionary = a.copy()
         self.val = array
+
+    def check_telephone(self)-> bool:
+        """для проверки номера телефона"""
+        pat = "\\+(7)\\-\\(\\d{3}\\)\\-\\d{3}\\-\\d{2}\\-\\d{2}$"
+        if re.math(pat, self.dictionary["telephone"]):
+            return True
+        return False
+
+    def check_height(self) -> bool:
+        """для проверки роста"""
+        if 1.00 < self.dictionary["height"] < 2.50:
+            return True
+        return False
+
+    def check_inn(self) -> bool:
+        """для проверки инн"""
+        pat = "^//d{12}"
+        if re.math(pat, self.dictionary["inn"]):
+            return True
+        return False
+
+    def check_passport_number(self) -> bool:
+        """для проверки номера паспорта"""
+        pat = "^//d{6}"
+        if re.math(pat, self.dictionary["passport_number"]):
+            return True
+        return False
+
+    def check_university(self) -> bool:
+        """для проверки наименования университета"""
+        pat = "^.*([Уу]ниверситет|[Аа]кадеми[яи]|[Пп]олитехнический|[И|и]нститут|им.|[А-Я]{3,}|РФ|СП[Бб]ГУ|[Пп]олитех|[Ии]сследовательск[А-Я]{2}.*$)"
+        if re.math(pat, self.dictionary["university"]):
+            return True
+        return False
+
+    def check_age(self) -> bool:
+        """для проверки возраста"""
+        if 0 <= self.dictionary["age"] < 120:
+            return True
+        return False
+
+    def check_academic_degree(self) -> bool:
+        pat = "Бакалавр|Специалист|Магистр|Кандидат наук|Доктор наук"
+        if re.math(pat, self.dictionary["academic_degree"]):
+            return True
+        return False
+
+    def check_worldview(self)-> bool:
+        """для вероисповедания"""
+        pat = ""
+        if re.math(pat, self.dictionary["worldview"]):
+            return True
+        return False
+
+    def check_address(self)-> bool:
+        """для адреса проживания"""
+        pat = ""
+        if re.math(pat, self.dictionary["address"]):
+            return True
+        return False
 
     def check_valid(self):
         result = {}
+
 
         return result
 
