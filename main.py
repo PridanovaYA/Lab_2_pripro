d8ef78e7ab923822a0ca29b5c290d580191bd811
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2021-12-05 01:25:21 +0400

    Работа с аргументами командной строки, реализованная с помощью argparse

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import argparse
+
 from Reader import Reader
 from Valid import Valid
+
 
 filename = "16.txt"
 file_path = "D://Lab_2_pripro/16.txt"
 
+parser = argparse.ArgumentParser("Input/Output parser")
+parser.add_argument("-input", type=str, default="16.txt", help="Input path")
+parser.add_argument("-output", type=str, default="res.txt", help="Output path")
+par = parser.parse_args()
 
+f = Reader(par.input)
+print(f)
 
